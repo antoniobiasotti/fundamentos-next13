403389477b1bf5dc893c7f059253df3b167f64f2
antoniobiasotti <abamorim36@gmail.com>
2024-04-12 17:19:01 -0300

    Loading.jsx on Root and More SSR

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,5 @@
+export default async function Catalog() {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    
+    return <h1>Catalog</h1>
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,3 @@
+export default function RootLoading() { 
+    return <p>Carregando...</p>
+}
